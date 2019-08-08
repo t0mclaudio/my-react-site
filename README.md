423eb59264d55344e93d4c09c6447f84d681d7c4
Tom Claudio <t0mclaudio@Toms-MacBook-Pro.local>
2019-08-09 07:17:41 +0800

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-# t0mclaudio.github.io
-## This would be my website
-### Coming Soon!
---
+++ precache-manifest.4e146ffc34e744f46e98421c05c03008.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a052c06b58d26c4b0bc2d41a3e3934f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2abb54ec8cb1ad5f05da",
+    "url": "/static/css/main.cbef78e9.chunk.css"
+  },
+  {
+    "revision": "b5c5beb43fab1ff7f479",
+    "url": "/static/js/2.ec295295.chunk.js"
+  },
+  {
+    "revision": "2abb54ec8cb1ad5f05da",
+    "url": "/static/js/main.d8acac5a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a17abd0604f2ec6f4bafd91d82ef96de",
+    "url": "/static/media/gotham-rounded-bold.a17abd06.otf"
+  },
+  {
+    "revision": "e40570a322c8676a5c07b3630c4cfee5",
+    "url": "/static/media/gotham-rounded-book.e40570a3.otf"
+  }
+]);
