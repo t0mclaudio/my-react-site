0e520e1e0bf8a365393b6807fa9720597b06cf88
Tom Claudio <526473+gb96@users.noreply.github.com>
2019-11-29 16:19:57 +0800

    Re initialize react project

---
+++ src/About.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default () => (
+  <div className="App">
+    <h1>Tom Claudio</h1>
+    <h2>Software Developer</h2>
+    <h3>About Me</h3>
+    <div className='about-me'>
+      <p>I like to work on projects that benefit people - that is how I perceive my value to the things I am working on and to the people I am working with. I believe that great digital transformation efforts are focused on improving the way we live.</p>
+      <p>My knowledge in web technologies, while transient, allows me to participate in these efforts of creating something that people can actually use. The transient nature of technology constantly allows me to relearn fundamentals while learning and trying new ways of doing things. I trust the accumulation of these knowledge, experience, skills and the proper mindset allows me to push the boundaries of what can be accomplished.</p>
+      <p>I believe programming languages, frameworks, and libraries can be learned and complex problems can be solved, however, the willingness to go at it is an attitude that I constantly cultivate.</p>
+      <p>I believe greater things can be achieved working with like minded people with a clear vision of what needs to be accomplished.</p>
+      <p>I want to be a person of value to the team and organisation I am with - doing my part, no matter how meager, to make sure that the people around me and the organisation succeeds and gets to the next level.</p>
+      <p>I have more than 8 years experience as a web developer and application programmer in the travel and tourism industry.</p>
+      <p>Skills: Full Stack Web Developer, Front End Web Developer, Back End Web Developer, API Developer, Cloud Developer</p>
+      <p>Programming Languages: Python, Javascript, PHP</p>
+      <p>Frameworks: Serverless Framework, Django, Flask</p>
+      <p>Libraries: ReactJS, VueJS</p>
+      <p>Tools: Sketch App</p>
+      <p>Other: Firebase, AWS Lambda, AWS Beanstalk, AWS Serverless</p>
+    </div>
+  </div>
+);
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import About from './About';
+
+const root = document.getElementById('root');
+
+ReactDOM.render(<About />, root)
