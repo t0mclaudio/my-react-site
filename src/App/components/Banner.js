18bf701f18f86365756b259d4ad06fc42b3d846d
Tom Claudio <526473+gb96@users.noreply.github.com>
2019-11-30 18:41:45 +0800

    Create Banner component with test

---
+++ src/App/components/Banner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default () => (
+  <section id="banner">
+    <div>
+      <img id="photo" src="img/tom.jpeg" />
+      <h2 className="m-0">Tom Claudio</h2>
+      <small className="m-0">Senior Front End Engineer</small>
+    </div>
+    <p id="lead-text">Tom is a frontend engineer with 10+ years experience building full stack web applications.</p>
+  </section>
+);
--- src/App/tests/Banner.test.js
+++ src/App/tests/Banner.test.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import expect from 'expect';
+import { shallow, configure } from 'enzyme';
 import { create } from 'react-test-renderer';
+import Banner from '../components/Banner';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
 
 describe('Banner Component', () => {
   it('Banner renders', () => {
-    const banner = create(<Banner />);
+    let banner = create(<Banner />);
     expect(banner.toJSON()).toMatchSnapshot();
+  });
+  it('Renders banner component', () => {
+    let banner = shallow(<Banner />);
+    expect(banner.find('img').length).toEqual(1);
+    expect(banner.find('h2').length).toEqual(1); 
+    expect(banner.find('p').length).toEqual(1); 
   })
 })
