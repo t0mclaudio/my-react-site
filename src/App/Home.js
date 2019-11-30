9978cb3562ce1b87605e6d3f5fabe7a1e7479dea
Tom Claudio <526473+gb96@users.noreply.github.com>
2019-11-30 14:26:08 +0800

    Add Banner and Cards

--- src/App/Home.js
+++ src/App/Home.js
@@ -7,11 +7,44 @@ export default () => (
       <Link to="/about-me">About Me</Link>
     </section>
     <section id="banner">
-      <h2>Tom Claudio</h2>
-      <h3>Senior Front End Engineer</h3>
-      <p>Tom is a frontend engineer with 10 years experience developing and deploying full stack web applications.</p>
+      <div>
+        <img id="photo" src="img/tom.jpeg" />
+        <h2 className="m-0">Tom Claudio</h2>
+        <small className="m-0">Senior Front End Engineer</small>
+      </div>
+      <p id="lead-text">Tom is a frontend engineer with 10+ years experience building full stack web applications.</p>
     </section>
     <section id="portfolio">
+      <div className="card">
+        <div className="card-img-container">
+          <img src="" alt="" />
+        </div>
+          <div className="container">
+            <h4><b>Fresh Studio Website</b></h4>
+            <p>Fresh Studio</p>
+            <p>Html, CSS, Joomla, Php</p>
+          </div>
+      </div>
+      <div className="card">
+        <div className="card-img-container">
+          <img src="" alt="" />
+        </div>
+          <div className="container">
+            <h4><b>Fresh Studio Website</b></h4>
+            <p>Fresh Studio</p>
+            <p>Html, CSS, Joomla, Php</p>
+          </div>
+      </div>
+      <div className="card">
+        <div className="card-img-container">
+          <img src="" alt="" />
+        </div>
+          <div className="container">
+            <h4><b>Fresh Studio Website</b></h4>
+            <p>Fresh Studio</p>
+            <p>Html, CSS, Joomla, Php</p>
+          </div>
+      </div>
     </section>
   </div>
 );
