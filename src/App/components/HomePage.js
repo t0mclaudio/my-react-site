3c55c3a29ab2903e753d20cca3c3b584b3c4bac2
Tom Claudio <526473+gb96@users.noreply.github.com>
2019-11-30 18:42:21 +0800

    Extract Banner from HomePage

--- src/App/components/HomePage.js
+++ src/App/components/HomePage.js
@@ -6,14 +6,6 @@ export default () => (
     <section id="navbar">
       <Link to="/about-me">About Me</Link>
     </section>
-    <section id="banner">
-      <div>
-        <img id="photo" src="img/tom.jpeg" />
-        <h2 className="m-0">Tom Claudio</h2>
-        <small className="m-0">Senior Front End Engineer</small>
-      </div>
-      <p id="lead-text">Tom is a frontend engineer with 10+ years experience building full stack web applications.</p>
-    </section>
     <section id="portfolio">
       <div className="card">
         <div className="card-img-container">
