d48500ceefe93cdc8670be8fc611e64147b0e728
Tom Claudio <526473+gb96@users.noreply.github.com>
2019-12-01 20:06:07 +0800

    Fix test

--- src/App/tests/HomePage.test.js
+++ src/App/tests/HomePage.test.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import expect from 'expect';
 import { shallow, mount, configure } from 'enzyme';
-import HomePage from '../components/HomePage';
 import Adapter from 'enzyme-adapter-react-16';
 import { BrowserRouter as Router } from 'react-router-dom';
+
+import HomePage from '../components/HomePage';
+import Banner from '../components/Banner';
+import Portfolio from '../components/Portfolio';
 
 configure({ adapter: new Adapter() });
 
 describe('Test Home Page', () => {
   it("Contains Banner & Portfolio Component", () => {
-    let home = mount(<Router><HomePage /></Router>);
-    expect(home.find('#banner').length).toEqual(1);
-    expect(home.find('#portfolio').length).toEqual(1);
+    let home = shallow(<HomePage />);
+    expect(home.contains(<Banner />)).toBe(true);
+    expect(home.contains(<Portfolio />)).toBe(true);
   });
 })
