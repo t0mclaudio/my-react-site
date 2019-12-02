204fe2e3541fbd5bcf1c6c71f71d9304a4956d59
Tom Claudio <526473+gb96@users.noreply.github.com>
2019-12-02 15:45:19 +0800

    Create initialState for App

---
+++ src/App/initialState.js
@@ -0,0 +1,29 @@
+export default [
+  {
+    name: 'Project 1',
+    short: 'This is going to be a short description',
+    long: 'This is the longer description',
+    technologies: ['html', 'css', 'react'],
+    repo: 'the repository link',
+    demo: 'demo link',
+    img: 'image url',
+  },
+  {
+    name: 'Project 1',
+    short: 'This is going to be a short description',
+    long: 'This is the longer description',
+    technologies: ['html', 'css', 'react'],
+    repo: 'the repository link',
+    demo: 'demo link',
+    img: 'image url',
+  },
+  {
+    name: 'Project 1',
+    short: 'This is going to be a short description',
+    long: 'This is the longer description',
+    technologies: ['html', 'css', 'react'],
+    repo: 'the repository link',
+    demo: 'demo link',
+    img: 'image url',
+  },
+];
