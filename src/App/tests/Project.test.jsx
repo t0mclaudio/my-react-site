14ba65cdc5aa4b44c0457b118b19676239af1653
Tom Claudio <526473+gb96@users.noreply.github.com>
2019-12-02 21:01:35 +0800

    Update tests to accommodate Redux

--- src/App/tests/Project.test.jsx
+++ src/App/tests/Project.test.jsx
@@ -3,12 +3,14 @@ import expect from 'expect';
 import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import Project from '../components/Project';
+import initialState from '../initialState';
 
 configure({ adapter: new Adapter() });
 
 describe('Project component test', () => {
   it('Renders correctly', () => {
-    const wrapper = shallow(<Project />);
+    const proj = initialState.projects[0];
+    const wrapper = shallow(<Project key={proj.name} proj={proj} />);
     expect(wrapper.find('.project')).toHaveLength(1);
   });
 });
--- src/App/tests/Projects.test.jsx
+++ src/App/tests/Projects.test.jsx
@@ -1,14 +1,27 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import expect from 'expect';
-import { shallow, configure } from 'enzyme';
+import { mount, configure } from 'enzyme';
+import { createStore } from 'redux';
 import Adapter from 'enzyme-adapter-react-16';
+import { Provider } from 'react-redux';
+import initialState from '../initialState';
+import appReducers from '../reducers';
 import Projects from '../components/Projects';
 
 configure({ adapter: new Adapter() });
 
+const store = createStore(appReducers, initialState);
+
+const CustomProvider = ({ children }) => (
+  <Provider store={store}>
+    {children}
+  </Provider>
+);
+
 describe('Test Projects Component', () => {
   it('Projects Component mounts', () => {
-    const projects = shallow(<Projects />)
+    const projects = mount(<CustomProvider><Projects /></CustomProvider>);
     expect(projects.find('#projects').length).toEqual(1);
   });
 });
