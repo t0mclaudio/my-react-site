f6b4fe789993483303b33cd6407f3af413810808
Tom Claudio <526473+gb96@users.noreply.github.com>
2019-12-02 21:01:20 +0800

    Cleanup Projects components

--- src/App/components/Projects.js
+++ src/App/components/Projects.js
@@ -1,19 +1,19 @@
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/prop-types */
 import React from 'react';
 import { connect } from 'react-redux';
 
 import Project from './Project';
 
-
 const Projects = (props) => (
   <section id="projects">
     {
-      props.projects.map((proj, i) => (
-        <Project key={i} />
+      props.projects.map((proj) => (
+        <Project key={proj.name} proj={proj} />
       ))
     }
   </section>
 );
 
 const mapStateToProps = (props) => ({ projects: props.projects });
-
 export default connect(mapStateToProps)(Projects);
