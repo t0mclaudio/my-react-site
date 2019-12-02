281b500d3da3a57d8cc3784b48a9d38ac85b158e
Tom Claudio <526473+gb96@users.noreply.github.com>
2019-12-02 17:12:28 +0800

    Update initialState

--- src/App/initialState.js
+++ src/App/initialState.js
@@ -1,29 +1,32 @@
-export default [
-  {
-    name: 'Project 1',
-    short: 'This is going to be a short description',
-    long: 'This is the longer description',
-    technologies: ['html', 'css', 'react'],
-    repo: 'the repository link',
-    demo: 'demo link',
-    img: 'image url',
-  },
-  {
-    name: 'Project 1',
-    short: 'This is going to be a short description',
-    long: 'This is the longer description',
-    technologies: ['html', 'css', 'react'],
-    repo: 'the repository link',
-    demo: 'demo link',
-    img: 'image url',
-  },
-  {
-    name: 'Project 1',
-    short: 'This is going to be a short description',
-    long: 'This is the longer description',
-    technologies: ['html', 'css', 'react'],
-    repo: 'the repository link',
-    demo: 'demo link',
-    img: 'image url',
-  },
-];
+export default {
+  projects:
+    [
+      {
+        name: 'Project 1',
+        short: 'This is going to be a short description',
+        long: 'This is the longer description',
+        technologies: ['html', 'css', 'react'],
+        repo: 'the repository link',
+        demo: 'demo link',
+        img: 'image url',
+      },
+      {
+        name: 'Project 1',
+        short: 'This is going to be a short description',
+        long: 'This is the longer description',
+        technologies: ['html', 'css', 'react'],
+        repo: 'the repository link',
+        demo: 'demo link',
+        img: 'image url',
+      },
+      {
+        name: 'Project 1',
+        short: 'This is going to be a short description',
+        long: 'This is the longer description',
+        technologies: ['html', 'css', 'react'],
+        repo: 'the repository link',
+        demo: 'demo link',
+        img: 'image url',
+      },
+    ],
+};
