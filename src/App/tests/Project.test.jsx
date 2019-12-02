be78dd7968a4660ad05bc228afedb98210033891
Tom Claudio <526473+gb96@users.noreply.github.com>
2019-12-02 14:52:56 +0800

    Remove import of describe and it

--- src/App/tests/Project.test.jsx
+++ src/App/tests/Project.test.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import expect from 'expect';
-import { describe, it } from 'jest';
 import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import Project from '../components/Project';
