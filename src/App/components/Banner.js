389e63c805ad57c46f142f0c7ebf46f4e4024c46
Tom Claudio <526473+gb96@users.noreply.github.com>
2019-12-09 15:06:01 +0800

    Update social media links role aria-label and style

--- src/App/components/Banner.js
+++ src/App/components/Banner.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
 import React from 'react';
 import {
   FaLinkedinIn,
@@ -12,11 +13,34 @@ export default () => (
       <h2 className="m-0">Tom Claudio</h2>
       <small className="m-0">Senior Front End Engineer</small>
     </div>
-    <p id="lead-text">Tom is a frontend engineer with 10+ years experience building full stack web applications.</p>
-    <div>
-      <FaLinkedinIn />
-      <FaTwitterSquare />
-      <FaGithub />
+    <div id="bannerContent">
+      <p id="lead-text">Tom is a frontend engineer with 10+ years experience building full stack web applications.</p>
+      <div id="sm-links">
+        <a
+          href="https://www.linkedin.com/in/tomclaudio/"
+          className="sm-icon"
+          role="button"
+          aria-label="Go to Tom's LinkedIn page"
+        >
+          <FaLinkedinIn />
+        </a>
+        <a
+          href="https://twitter.com/t0mclaudio"
+          className="sm-icon"
+          role="button"
+          aria-label="Go to Tom's Twitter Page"
+        >
+          <FaTwitterSquare />
+        </a>
+        <a
+          href="https://github.com/t0mclaudio"
+          className="sm-icon"
+          role="button"
+          aria-label="Go to Tom's Github page"
+        >
+          <FaGithub />
+        </a>
+      </div>
     </div>
   </section>
 );
